c8097e0b51828c43ce43b773d255ecb8c7257bdb
tquean15012003 <tquean15012003@gmail.com>
2023-02-27 13:41:06 +0800

    add login page and modify the template

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -7,26 +7,21 @@ export default function Homepage() {
     const navigate = useNavigate()
 
     return (
-        <Container maxW="8xl" paddingTop="5">
-            <div style={{ display: "flex", justifyContent: "right", alignItems: "center" }}>
-                <Box>
-                    <Button onClick={() => {
-                        navigate("/login", { replace: false })
-                    }} size="lg" colorScheme="yellow" color="black" ><Text fontSize="xl">Login</Text></Button>
-                </Box>
-            </div>
-            <Box marginTop="20">
+        <Box>
+            <Box marginTop="10">
                 <Text fontWeight="bold" color="black" fontSize="6xl" noOfLines="2">Because you deserve better than toxic groupmates...</Text>
             </Box>
             <Box marginTop="20">
-                <Text fontWeight="bold" color="yellow.400" fontSize="5xl" noOfLines="2">Your ideal groupmate could be just one swipe away...</Text>
+                <Text fontWeight="bold" color="#ED9B59" fontSize="5xl" noOfLines="2">Your ideal groupmate could be just one swipe away...</Text>
             </Box>
             <Box marginTop="20">
-                <Button onClick={() => {
-                    navigate("/login", { replace: false })
-                }} size="lg" colorScheme="yellow" color="black" ><Text fontSize="2xl" padding="0 10px">Register now</Text></Button>
+                <button onClick={() => {
+                    navigate("/signup", { replace: false })
+                }} style={{ padding: "5px 40px", color: "white", backgroundColor: "#ED9B59", borderRadius: "10px" }}
+                ><Text fontSize="3xl" fontWeight="bold">Register now</Text>
+                </button>
             </Box>
-        </Container>
+        </Box>
 
     )
 }
